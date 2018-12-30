5865047c8b0ef582f3eb7aa37ea9c79fc6dd9893
Rahul prajapati <33652351+Raarav@users.noreply.github.com>
2018-12-30 00:36:23 -0800

    find image detail

--- opencv/first.py
+++ opencv/first.py
@@ -9,10 +9,17 @@ import cv2
 
 def main():
     imgpath = "C:\\Users\\Rahul\\Pictures\\photo\\me.jpg" #image path
-    img = cv2.imread(imgpath) #image read
-    cv2.imshow('me',img)#image show
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    img = cv2.imread(imgpath,0) #image read
+    print(img)# print image in number format
+    print(type(img))#type(object) for type of image 
+    print(img.dtype)#object.dtype for print data type
+    print(img.shape)#object.shape for print shape of the image
+    print(img.ndim)#object.ndim for total dimension persent in image
+    print(img.size)#object.size print the pixel size of image 
+    
+ #   cv2.imshow('me',img)#image show
+ #   cv2.waitKey(0)
+  #  cv2.destroyAllWindows()
     
 if __name__ == "__main__":
     main()
